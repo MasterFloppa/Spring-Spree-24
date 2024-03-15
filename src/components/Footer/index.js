8ccab54dae4061335c7ea936972f7a4866e40fed
Raghunandan <nandan290803@gmail.com>
2024-03-15 13:39:50 +0530

    updated sponsors page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,13 +15,13 @@ export default function Footer() {
               />
               <div className="w-auto h-auto flex flex-col absolute ">
                 <div className="text flex-col justify-center items-center mb-8">
-                  <h3 className="text-2xl flex justify-center mt-20 mb-4 font-[800] xsm:text-center md:text-left text-purple">
+                  <h3 className="text-2xl flex justify-center mt-20 mb-4 font-[800] xsm:text-center md:text-left text-purple mt-60 text-4xl">
                     Rasengan
                   </h3>
-                  <h3 className="text-xl mb-2 italic font-semibold text-purple xsm:text-center md:text-left flex justify-center">
+                  <h3 className="text-xl mb-2 italic font-semibold text-purple xsm:text-center md:text-left flex justify-center text-3xl">
                     Where the culture resonates
                   </h3>
-                  <h3>
+                  <h3 className="md:w-4/5 text-xl ml-32 flex justify-center items-center text-center">
                     Namaste and a very warm welcome to SpringSpree - our vibrant
                     and colorful cultural fest celebrating the rich artistic
                     traditions of our community!
@@ -29,35 +29,35 @@ export default function Footer() {
                 </div>
                 <div className="flex-col justify-center items-center mb-4">
                   <div>
-                    <h3 className="block uppercase flex justify-center text-purple text-lg font-semibold">
+                    <h3 className="block uppercase flex justify-center text-purple text-lg font-semibold md: text-xl">
                       USEFUL LINKS
                     </h3>
                     <div className="w-2/5 ml-[320px] flex items-center justify-around">
-                      <div className="Links cursor-pointer text-purple font-medium text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
+                      <div className="Links cursor-pointer text-purple font-medium text-lg md:text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2 ">
                         <NavLink to="/events">Events</NavLink>
                       </div>
-                      <div className="Links cursor-pointer text-purple font-medium text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
+                      <div className="Links cursor-pointer text-purple font-medium text-lg md:text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
                         <NavLink to="/sponsors">Sponsors</NavLink>
                       </div>
-                      <div className="Links cursor-pointer text-purple font-medium text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
+                      <div className="Links cursor-pointer text-purple font-medium text-lg md:text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
                         <NavLink to="/blogs">Blogs</NavLink>
                       </div>
-                      <div className="Links cursor-pointer text-purple font-medium text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
+                      <div className="Links cursor-pointer text-purple font-medium text-lg md:text-lg hover:text-purple font-weight-600 block pb-2 text-md pt-2">
                         <NavLink to="/faq">FAQS</NavLink>
                       </div>
                     </div>
                   </div>
                   <div className="flex-col justify-center items-center mb-10">
-                    <span className="flex justify-center items-center block uppercase text-purple text-lg font-semibold my-2">
+                    <span className="flex justify-center items-center block uppercase text-purple text-lg font-semibold my-2 md: text-xl">
                       Venue
                     </span>
                     <div>
-                      <ul className="flex-col justify-center items-center list-unstyled xsm:text-center sm:text-left">
-                        <li className="flex justify-center tracking-wide text-purple font-medium">
+                      <ul className="flex-col justify-center items-center list-unstyled md:text-lg xsm:text-center sm:text-left">
+                        <li className="flex justify-center tracking-wide text-purple font-medium md:text-lg">
                           National Institute of Technology Warangal, Hanamkonda
                           Road, 506004
                         </li>
-                        <li className=" flex justify-center text-purple font-medium">
+                        <li className=" flex justify-center text-purple font-medium md:text-lg">
                           springspree@nitw.ac.in
                         </li>
                       </ul>
@@ -66,7 +66,7 @@ export default function Footer() {
                   <div className="flex justify-between ml-48 mb-10">
                     <div id="horz-list" className="">
                       <ul>
-                        <li className="rounded-full overflow-hidden transition duration-300 hover:bg-purple rounded-lg py-1 hover:text-pink ">
+                        <li className="rounded-full overflow-hidden transition duration-300 md:text-xl hover:bg-purple rounded-lg py-1 hover:text-pink ">
                           <a
                             href="https://merchant.razorpay.com/policy/LSwZLjiieyrJiX/privacy"
                             className="block py-2 px-4"
@@ -74,7 +74,7 @@ export default function Footer() {
                             Privacy Policy
                           </a>
                         </li>
-                        <li className="rounded-full overflow-hidden transition duration-300 hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
+                        <li className="rounded-full overflow-hidden transition duration-300 md:text-xl hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
                           <a
                             href="https://merchant.razorpay.com/policy/LSwZLjiieyrJiX/terms"
                             className="block py-2 px-4"
@@ -82,7 +82,7 @@ export default function Footer() {
                             Terms & Conditions
                           </a>
                         </li>
-                        <li className="rounded-full overflow-hidden transition duration-300 hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
+                        <li className="rounded-full overflow-hidden transition duration-300 md:text-xl hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
                           <a
                             href="https://merchant.razorpay.com/policy/LSwZLjiieyrJiX/refund"
                             className="block py-2 px-4"
@@ -90,7 +90,7 @@ export default function Footer() {
                             Cancellation & Refund
                           </a>
                         </li>
-                        <li className="rounded-full overflow-hidden transition duration-300 hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
+                        <li className="rounded-full overflow-hidden transition duration-300 md:text-xl hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
                           <a
                             href="https://merchant.razorpay.com/policy/LSwZLjiieyrJiX/shipping"
                             className="block py-2 px-4"
@@ -98,7 +98,7 @@ export default function Footer() {
                             Shipping & Delivery
                           </a>
                         </li>
-                        <li className="rounded-full overflow-hidden transition duration-300 hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
+                        <li className="rounded-full overflow-hidden transition duration-300 md:text-xl hover:bg-purple rounded-lg py-1 hover:text-pink mt-2">
                           <a
                             href="https://merchant.razorpay.com/policy/LSwZLjiieyrJiX/contact_us"
                             className="block py-2 px-4"
@@ -112,10 +112,10 @@ export default function Footer() {
 
                   <hr className="w-full my-4 border-black"></hr>
                   <div className="flex justify-center items-center">
-                    <div className="text-sm text-purple font-semibold py-1 ">
+                    <div className="text-sm text-purple font-semibold py-1 md:text-lg ">
                       All rights reserved ©{" "}
                       <span id="get-current-year">2024</span>
-                      <span className="text-purple hover:text-gray-800">
+                      <span className="text-purple hover:text-gray-800 md:text-lg">
                         {" "}
                         SpringSpree'24 Dev Team{" "}
                       </span>
