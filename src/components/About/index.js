3b4100a5ae6bb7a51e95c72ba8be639817491cc1
K-S-Abhihkeshav <97213738+K-S-Abhihkeshav@users.noreply.github.com>
2024-03-19 16:17:30 +0530

    design and animation changes
    petals animation changed. added new logos and title to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,23 +9,27 @@ export const About = () =>{
                     <br />
                     <h1 className="flex-[100%] text-center text-4xl font-bold uppercase bg-gradient-to-r from-[#FFFFFF] to-[#F7CA17] inline-block text-transparent pb-5 bg-clip-text">About SpringSpree</h1>
                     <div className="sm:w-[100%] md:w-[45%] flex justify-center items-center">
-                        <img className="sm:w-[156px] lg:w-[296px] h-auto flex-shrink-1" src="../../../images/SpringSpree_logo.png" alt="spreelogo"/>
+                        <img className="sm:w-[156px] lg:w-[296px] h-auto flex-shrink-1" src="../../../images/SpringSpree2.png" alt="spreelogo"/>
                     </div>
                     <div className="about-content sm:w-[100%] md:w-[45%]">
-                    Spring Spree, the annual cultural festival of NIT Warangal, commenced in 1978 as a requisite break for the students from their enervatingand monotonous class schedules and didactic courses. 37 years on, it has been established as one of the best cultural festivals in the nation, enticing massive participation from the very center to the remote corners. Over theyears, we have had famous and ingenious artists, highlighting the festival with their esteemed presence and their majestic performances.
+                    <span className="text-[#ca85b8] font-bold">SpringSpree, NIT Warangal's annual cultural festival</span>, started in 1978 as a refreshing break from
+classes. Now, after 45 years, it's renowned as one of the nation's best cultural festivals, attracting
+participants from all corners. Over time, we've hosted famous artists, enriching the festival with their
+esteemed presence and performances.
                     </div>
                 </div>
                 <div className="about-theme flex flex-row-reverse flex-wrap justify-center justify-items-center items-center gap-4 p-4 mb-10">
                     <h1 className="flex-[100%] text-center font-bold text-4xl uppercase bg-gradient-to-r from-[#FFFFFF] to-[#F7CA17] inline-block text-transparent pb-5 bg-clip-text">The Theme</h1>
                     <div className="sm:w-[100%] md:w-[45%] flex justify-center items-center">
-                        <img className="sm:w-[256px] lg:w-[456px] h-auto flex-shrink-1 " src="../../../images/Kaladhvani_logo.png" alt="themelogo"/>
+                        <img className="sm:w-[330px] lg:w-[590px] h-auto flex-shrink-1 " src="../../../images/Rasengan.png" alt="themelogo"/>
                     </div>
                     <div className="theme-content sm:w-[100%] md:w-[45%]">
-                    <span className="text-[#F7CA17] font-bold">"Kala-dhvani"</span> is a term from Indian classical music that refers to the sound or resonance that is produced by the artist's instrument. It is believed that the quality of this sound or resonance is an important aspect of the music and can greatly affect the emotional impact it has on the listener.
-                    <br/>
-                    <span className="text-[#F7CA17] font-bold">"Kala" means art or performance, while "dhvani" refers to sound or resonance. </span>Therefore, "kala-dhvani" can be interpreted as the artistic sound or resonance that is produced during a performance in Indian classical music.
-                    <br/>
-                    The theme <span className="text-[#F7CA17] font-bold">'Kaladhvani - sounds of art' </span>reflects the different art forms and sounds that one can find in our country. We aim to showcase the diversity of our culture through various events that we have planned for the fest.
+                    <span className="text-[#ca85b8] font-bold">Spring</span>, a season of vibrant renewal and boundless energy, explodes onto the scene at NITW with
+                    <span className="text-[#ca85b8] font-bold">RASENGAN, the theme for this year's SpringSpree!</span> Inspired by the iconic Naruto jutsu, RASENGAN
+                    embodies the concentrated potential that lies dormant within each individual, waiting to be unleashed
+                    and channeled into achieving great things. Just like the swirling sphere of chakra in Naruto's hands,
+                    this theme promises a festival brimming with creative energy, pushing participants to break past limitations and discover their own hidden strengths. Prepare to witness a majestic confluence of cultural celebration, artistic expression, and exhilarating competition, all ignited by the unleashed spirit of spring
+                    at <span className="text-[#ca85b8] font-bold">RASENGAN!</span>
                     </div>
                 </div>
             </div>
