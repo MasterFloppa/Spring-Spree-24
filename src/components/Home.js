3bf995d3778f5f408823cde74d442e063dbc10b0
K-S-Abhihkeshav <97213738+K-S-Abhihkeshav@users.noreply.github.com>
2024-04-05 02:11:13 +0530

    frontpage changes
    image of director as well as thanking him for the spree

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = (props) => {
             {/* <img src={top_bg} alt="top_bg" className="top_bg" style={{'position': 'absolute','right':'1%','opacity':'10%','height':'150%','width':'70%'}}/>
             <img src={bottom_bg} alt="bottom_bg" className="bottom_bg" style={{'position': 'absolute','top':'200%','left':'%','opacity':'10%','height':'150%','width':'70%'}}/> */}
             <Hero />
-            {/* <Cguest /> */}
+            <Cguest /> 
             <About />
         </>
     )
